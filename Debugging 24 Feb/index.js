488acd9fdd202d8aa273d43bbc38a5e552d863c4
Ana Ceaicovschi <ceaicovski94@gmail.com>
2021-02-26 11:33:25 +0200

    added the debugging exercises to the repo

---
+++ Debugging 24 Feb/index.js
@@ -0,0 +1,20 @@
+
+
+const starWarsApiUrl = 'https://swapi.dev/api/people';
+
+const getStarWarsPeople = (cb) => {
+  fetch(starWarsApiUrl)
+  .then(res => res.json())
+  .then(cb); ///=> console.log(cb))  --- also shows in console the object from API;
+}
+
+
+const output = document.querySelector('#info');
+
+getStarWarsPeople((res) => {
+  //console.log(res);
+  const firstPerson = res.results[0].name;
+  const totalNumber = res.results.length;
+
+  output.innerHTML = `The total number of persons: ${totalNumber} <br/> The name of the first person on the list: ${firstPerson}`;
+});
