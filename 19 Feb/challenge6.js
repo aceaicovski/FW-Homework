fae970b161a57527b3a0aa7c68aa8a8d33024d9f
Ana Ceaicovschi <ceaicovski94@gmail.com>
2021-03-01 11:47:58 +0200

    changed challenge6.js

--- 19 Feb/challenge6.js
+++ 19 Feb/challenge6.js
@@ -4,8 +4,6 @@
 // проверить если у нашего obj  есть ключ info и в нем ключ name , 
 // то мы возвращаем значение из этого ключа. (Вложенность может мыть любая к примеру info.data.roles[2])
 
-<<<<<<< HEAD:19 Feb/challenge6.js
-=======
 // function getValueByPath(obj, path) {
 
 //     for ( let path in obj) {
@@ -14,7 +12,6 @@
 //     }}
 // };
 
->>>>>>> ee94452d52974b4aa874cfd2c875084b603b5fac:challenge6.js
 
 const obj = {
     foo: { bar: 'baz' }
@@ -35,12 +32,6 @@ function getValueByPath(obj, path) {
     return getValueByPath(obj[parts[0]], parts.slice(1).join("."));
 }
 
-<<<<<<< HEAD:19 Feb/challenge6.js
 console.log(getValueByPath(obj, 'foo.bar'));
 console.log(getValueByPath(obj1, 'parameters.weight'));
 console.log(getValueByPath(obj1, 'parameters.height'));
-=======
-console.log(recLookup(obj, 'foo.bar'));
-console.log(recLookup(obj1, 'parameters.weight'));
-console.log(recLookup(obj1, 'parameters.height'));
->>>>>>> ee94452d52974b4aa874cfd2c875084b603b5fac:challenge6.js
